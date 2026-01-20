14fdfdb339eb84bdef83307eba75616c8c82dcd4
EzraAmani <ezraamani02@gmail.com>
2026-01-20 16:51:43 +0300

    new print

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
-public class Main{
+public class Main {
 
-public static void main(String[] args){
-    System.out.println("Hello World"); 
-}
+    public static void main(String[] args) {
+        System.out.println("Hello World");
 
+        System.out.println("New World");
+    }
 
 }
